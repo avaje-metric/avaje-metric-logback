155a09abcbba7470f9481b43107300266e515180
rob bygrave <robin.bygrave@gmail.com>
2018-10-12 02:33:26 +1300

    Update for 7.2.1

--- src/main/java/org/avaje/metric/logback/LogMetricAppender.java
+++ src/main/java/org/avaje/metric/logback/LogMetricAppender.java
@@ -1,34 +1,45 @@
 package org.avaje.metric.logback;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import org.avaje.metric.CounterMetric;
 import org.avaje.metric.MetricManager;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import org.slf4j.LoggerFactory;
 
 /**
- * Appender that counts error, warning and info events that are sent to it.
- * <p>
- * Lower level events (DEBUG, TRACE) are deemed uninteresting.
- * </p>
+ * Appender that counts errors and warnings.
  */
 public final class LogMetricAppender extends AppenderBase<ILoggingEvent> {
+
+  /**
+   * Register the LogMetricAppender to the ROOT logger.
+   */
+  public static void register() {
+
+    LoggerContext factory = (LoggerContext) LoggerFactory.getILoggerFactory();
+    Logger root = factory.getLogger(Logger.ROOT_LOGGER_NAME);
+
+    LogMetricAppender appender = new LogMetricAppender();
+    appender.setContext(root.getLoggerContext());
+    appender.start();
+
+    root.addAppender(appender);
+  }
 
   private final CounterMetric errorMetric;
 
   private final CounterMetric warnMetric;
-  
-  private final CounterMetric infoMetric;
 
   /**
    * Create with a given rateUnit.
    */
   public LogMetricAppender() {
-
     errorMetric = MetricManager.getCounterMetric("app.log.error");
     warnMetric = MetricManager.getCounterMetric("app.log.warn");
-    infoMetric = MetricManager.getCounterMetric("app.log.info");
   }
 
   /**
@@ -43,9 +54,6 @@ public final class LogMetricAppender extends AppenderBase<ILoggingEvent> {
       break;
     case Level.ERROR_INT:
       errorMetric.markEvent();
-      break;
-    case Level.INFO_INT:
-      infoMetric.markEvent();
       break;
     default:
       // not interested in any other logging events
